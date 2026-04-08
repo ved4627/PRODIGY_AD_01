e39d44d1cd9898655782a4d57178f769347096ad
BABA <vedansh@Vedanshs-MacBook-Pro.local>
2026-04-08 23:49:28 +0530

    Task 01: Math logic and oncreate fix

--- app/src/main/java/com/example/prodigy_ad_01/MainActivity.java
+++ app/src/main/java/com/example/prodigy_ad_01/MainActivity.java
@@ -10,32 +10,60 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView tvDisplay;
     private double firstValue = Double.NaN;
-    private double secondValue;
-    private String currentAction;
+    private String currentAction = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         tvDisplay = findViewById(R.id.tvDisplay);
     }
 
-    // Number Buttons Click Logic
     public void onNumberClick(View view) {
-        Button button = (Button) view;
-        String currentText = tvDisplay.getText().toString();
-
-        if (currentText.equals("0")) {
-            tvDisplay.setText(button.getText().toString());
+        Button b = (Button) view;
+        if (tvDisplay.getText().toString().equals("0")) {
+            tvDisplay.setText(b.getText().toString());
         } else {
-            tvDisplay.append(button.getText().toString());
+            tvDisplay.append(b.getText().toString());
         }
     }
 
-    // Clear (AC) Logic
+    public void onOperatorClick(View view) {
+        try {
+            Button b = (Button) view;
+            firstValue = Double.parseDouble(tvDisplay.getText().toString());
+            currentAction = b.getText().toString();
+            tvDisplay.setText("0");
+        } catch (Exception e) {
+            tvDisplay.setText("Error");
+        }
+    }
+
+    public void onEqualClick(View view) {
+        try {
+            if (!Double.isNaN(firstValue)) {
+                double secondValue = Double.parseDouble(tvDisplay.getText().toString());
+                double result = 0;
+
+                if (currentAction.equals("+")) result = firstValue + secondValue;
+                else if (currentAction.equals("-")) result = firstValue - secondValue;
+                else if (currentAction.equals("*")) result = firstValue * secondValue;
+                else if (currentAction.equals("/")) {
+                    if (secondValue != 0) result = firstValue / secondValue;
+                    else { tvDisplay.setText("Error"); return; }
+                }
+
+                tvDisplay.setText(String.valueOf(result));
+                firstValue = Double.NaN;
+            }
+        } catch (Exception e) {
+            tvDisplay.setText("Error");
+        }
+    }
+
     public void onClearClick(View view) {
         tvDisplay.setText("0");
         firstValue = Double.NaN;
+        currentAction = "";
     }
 }
