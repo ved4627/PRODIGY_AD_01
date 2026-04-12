34683f289f79214cd44fb28312d9bc895d444b9f
Vedansh Gupta <9305875208ved@gmail.com>
2026-04-12 18:50:32 +0530

    Calculator UI updated with large buttons and full math logic

--- app/src/main/java/com/example/prodigy_ad_01/MainActivity.java
+++ app/src/main/java/com/example/prodigy_ad_01/MainActivity.java
@@ -21,18 +21,34 @@ public class MainActivity extends AppCompatActivity {
 
     public void onNumberClick(View view) {
         Button b = (Button) view;
-        if (tvDisplay.getText().toString().equals("0")) {
-            tvDisplay.setText(b.getText().toString());
+        String text = b.getText().toString();
+
+        if (tvDisplay.getText().toString().equals("0") || tvDisplay.getText().toString().equals("Error")) {
+            tvDisplay.setText(text);
         } else {
-            tvDisplay.append(b.getText().toString());
+
+            if (text.equals(".") && tvDisplay.getText().toString().contains(".")) {
+                return;
+            }
+            tvDisplay.append(text);
         }
     }
 
     public void onOperatorClick(View view) {
         try {
             Button b = (Button) view;
+            String op = b.getText().toString();
+
+            // Special logic for +/- button
+            if (op.equals("+/-")) {
+                double val = Double.parseDouble(tvDisplay.getText().toString());
+                val = val * -1;
+                tvDisplay.setText(String.valueOf(val));
+                return;
+            }
+
             firstValue = Double.parseDouble(tvDisplay.getText().toString());
-            currentAction = b.getText().toString();
+            currentAction = op;
             tvDisplay.setText("0");
         } catch (Exception e) {
             tvDisplay.setText("Error");
@@ -45,16 +61,25 @@ public class MainActivity extends AppCompatActivity {
                 double secondValue = Double.parseDouble(tvDisplay.getText().toString());
                 double result = 0;
 
-                if (currentAction.equals("+")) result = firstValue + secondValue;
-                else if (currentAction.equals("-")) result = firstValue - secondValue;
-                else if (currentAction.equals("*")) result = firstValue * secondValue;
-                else if (currentAction.equals("/")) {
-                    if (secondValue != 0) result = firstValue / secondValue;
-                    else { tvDisplay.setText("Error"); return; }
+                switch (currentAction) {
+                    case "+": result = firstValue + secondValue; break;
+                    case "-": result = firstValue - secondValue; break;
+                    case "*": result = firstValue * secondValue; break;
+                    case "/":
+                        if (secondValue != 0) result = firstValue / secondValue;
+                        else { tvDisplay.setText("Error"); return; }
+                        break;
+                    case "%": result = (firstValue / 100) * secondValue; break;
                 }
 
-                tvDisplay.setText(String.valueOf(result));
+
+                if (result == (long) result) {
+                    tvDisplay.setText(String.valueOf((long) result));
+                } else {
+                    tvDisplay.setText(String.valueOf(result));
+                }
                 firstValue = Double.NaN;
+                currentAction = "";
             }
         } catch (Exception e) {
             tvDisplay.setText("Error");
